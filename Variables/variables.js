baab119c1f3937f8f1475bbfaafc18ee0387d8a1
Andrez23 <marincarlosandres42@gmail.com>
2023-10-03 11:28:21 -0500

    Variables

---
+++ Variables/variables.js
@@ -0,0 +1,16 @@
+// Formas de definir variables
+//Var: es la forma antigua, no se recomienda usar en el codigo acutal
+
+var edad = 27; 
+console.log(edad);
+
+//Let: es la forma mas recomendable para declarar varib¿ables, debido a su alcance
+
+let nombre= "Andres";
+console.log(nombre);
+
+//const: esta es una constante, tambien se usa, y esta no debe cambiar su valor, es decir, no podrá actualizars en el futuro
+
+const correo = "correo@gmail.com";
+console.log(correo);
+
