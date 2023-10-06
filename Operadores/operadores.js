a295d12252863712731caf3e8e2edcb014c3c0fe
Andrez23 <marincarlosandres42@gmail.com>
2023-10-06 11:41:32 -0500

    Operadores

---
+++ Operadores/operadores.js
@@ -0,0 +1,92 @@
+//Operadores
+
+//Este nos permitira hacer calculos basicos y operaciones
+
+//Operadores aritmeticos
+
+ //Ejemplo de suma
+const resultado = 10 + 10 ;  
+console.log(resultado);  
+
+//Ejemplo de resta
+const resultado2 = 10 - 10 ; 
+console.log(resultado2);
+
+//Ejemplo de modulo
+const resultado3 = 10 % 3 ; 
+console.log(resultado3);
+
+//Ejemplo de aumento 
+let numero = 1;
+numero ++;
+
+console.log(numero);  
+
+
+//Operdadores de asignacion
+
+//Nos permite dar un valor a nuestra variable
+
+//+= suma un numero al valor de una varibale
+let num = 10;
+num += 2;
+
+console.log(num);
+
+//+= resta un numero al valor de una varibale
+let num2 = 10;
+num -= 2;
+
+console.log(num2);
+
+//*= multiplica un numero al valor de una varibale
+let num3 = 10;
+num *= 2;
+
+console.log(num3);
+
+// /= divide un numero al valor de una varibale
+let num4 = 10;
+num /= 2;
+
+console.log(num4);
+
+// % saca el sobrante de la division y lo asigna a la variable
+let num5 = 10;
+num %= 3;
+
+console.log(num5);
+
+
+//Operadores de comparacion
+
+//Nos permite comparar valores
+
+//mayor que 
+const res= 5 > 1;
+console.log(res);
+
+//menor que 
+const res1= 5 < 1;
+console.log(res1);
+
+//mayor o igual 
+const res2= 4 <= 4;
+console.log(res2);
+
+//menor o igual 
+const res3= 4 <= 4;
+console.log(res3);
+
+//igual que 
+const res4= 3 == 1;
+console.log(res4);
+
+//igual en valor y tipo de valor
+const res5= 4 === "4";
+console.log(res5);
+
+//diferente de 
+const res6=  3 != 2; 
+console.log(res6);
+
