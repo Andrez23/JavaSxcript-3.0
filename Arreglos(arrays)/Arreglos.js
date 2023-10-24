005e438fbd149239effb9c165c5923e9a8dd7826
Andrez23 <marincarlosandres42@gmail.com>
2023-10-24 12:05:59 -0500

    Arreglos

---
+++ Arreglos(arrays)/Arreglos.js
@@ -0,0 +1,10 @@
+//Arreglos (arrays)
+//Son variables especiales en las que podemos almacenar una coleccion o agrupacion de valores de diferente naturaleza en su interior
+
+const arreglo = ["texto", 456.1, false, {propiedad: "valor"}, [1,2,3]];
+console.log(arreglo); 
+
+//Como acceder a la informacion del arreglo?
+
+const amigos = ["Alejandro","Julian","Jose"];
+console.log(amigos[0]);
